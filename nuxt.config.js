3e8ffab32ddca66b65dfe4cfdb75600f5001c2c3
Kenneth Sun <kenhtsun@gmail.com>
2022-04-17 21:45:05 -0400

    minor

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,5 +83,5 @@ export default {
     routes() {
       return getRoutes();
     }
-  }
+  },
 };
--- utils/getRoutes.js
+++ utils/getRoutes.js
@@ -5,7 +5,7 @@ export default async () => {
     .fetch();
   return files.map((file) => {
     return {
-      url: 'blog/' + file.path.replace('index', ''),
+      url: 'projects/' + file.path.replace('index', ''),
       lastmod: Date.parse(file.updatedAt),
     };
   });
