8bee049a39a46f515e1512eb5adbfb2046c54d3b
Kenneth Sun <kenhtsun@gmail.com>
2022-07-20 00:37:35 -0400

    remove aggressive crawler behavior

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,12 +80,12 @@ export default {
   generate: {
     crawler: true,
     dir: 'generate',
-    async routes () {
-      const { $content } = require('@nuxt/content');
-      const files = await $content({ deep: true }).only(['path']).fetch();
+    // async routes () {
+    //   const { $content } = require('@nuxt/content');
+    //   const files = await $content({ deep: true }).only(['path']).fetch();
 
-      return files.map(file => file.path === '/index' ? '/' : file.path);
-    }
+    //   return files.map(file => file.path === '/index' ? '/' : file.path);
+    // }
   },
 
   sitemap: {
